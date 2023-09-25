e761c27b381fed8d1b4e06139403ca22d26c5100
duck1won <ejr3949@naver.com>
2023-09-25 19:29:38 +0900

    -마무리된일 표시하기 -조건부 삼항 연산자사용 -complete true인 목록에만 선 긋기

--- src/App.js
+++ src/App.js
@@ -2,6 +2,12 @@ import React, { Component } from "react";
 import './App.css';
 export default class App extends Component {
   //jsx에서는 이런식으로 css파일안에서 하는 형식처럼 선언을 해주어도 스타일을 지정해줄 수 있음
+
+  state = {
+    todoData: [],
+    value: "",
+  };
+
   btnStyle = {
     color: "#fff",
     border: "none",
@@ -10,29 +16,14 @@ export default class App extends Component {
     cursor: "pointer",
     float: "right"
   }
-  getStyle = () => {
+  //complete true인 목록에만 선 긋기
+  getStyle = (completed) => {
     return {
       padding: "10px",
       borderBottom: "1px #ccc dotted",
-      textDecoration: 'none'
+      textDecoration: completed ? 'line-through' : 'none'
     }
   }
-
-  state = {
-    todoData: [
-      {
-        id: "1",
-        title: "공부하기",
-        complete: true
-      },
-      {
-        id: "2",
-        title: "청소하기",
-        complete: false
-      }
-    ],
-    value: "",
-  };
 
   //filter메소드 사용하여 할일 목록 지워 콘솔로그 찍기
   handleClick = (id) => {
@@ -52,12 +43,23 @@ export default class App extends Component {
     let newTodo = {
       id: Date.now(),
       title: this.state.value,
-      complete: false,
+      completed: false,
     };
 
     //원래 있던 할 일에 새로운 할 일 더해주기
-    this.setState({ todoData: [...this.state.todoData, newTodo] });
+    this.setState({ todoData: [...this.state.todoData, newTodo], value: "" });
   }
+
+  handelCompleteChange = (id) => {
+    let newTodoData = this.state.todoData.map((data) => {
+      if (data.id === id) {
+        data.completed = !data.completed;
+      }
+      return data;
+    });
+    this.setState({ todoData: newTodoData });
+  };
+
   render() {
     return (
       //jsx에서는 html과 다르게 class뒤에 Name을 무조껀 붙혀야한다 'className'
@@ -67,13 +69,24 @@ export default class App extends Component {
             <h1>할 일 목록</h1>
           </div>
           {this.state.todoData.map((data) => ( //key속성을 넣어줘야한다 data 에서는 id값이 key에 해당함
-            <div style={this.getStyle()} key={data.id}>
-              <input type="checkbox" defaultchecked={false} />
-              {data.title}
-              <button style={this.btnStyle} onClick={() => this.handleClick(data.id)}>x</button>
+            //complete true인 목록에만 선 긋기
+            <div style={this.getStyle(data.completed)} key={data.id}>
+              <p>
+                <input
+                  type="checkbox"
+                  onChange={() => this.handelCompleteChange(data.id)}
+                  defaultchecked={false}
+                />
+                {data.title}
+                <button
+                  style={this.btnStyle}
+                  onClick={() => this.handleClick(data.id)}
+                >
+                  x
+                </button>
+              </p>
             </div>
           ))}
-
 
           <form style={{ display: 'flex' }} onSubmit={this.handelSubmit}>
             <input
