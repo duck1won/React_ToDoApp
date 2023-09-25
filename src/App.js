66bc4315383daf26934bebfeb9d36ec671a28015
duck1won <ejr3949@naver.com>
2023-09-25 19:03:01 +0900

    -할 일 목록을 입력하는 부분 추가 -입력 버튼 추가 -글 입력시 state 변경 -입력 버튼 클릭 시 목록에 추가하기, 입력란에 있던 글씨 지워주기

--- src/App.js
+++ src/App.js
@@ -30,16 +30,34 @@ export default class App extends Component {
         title: "청소하기",
         complete: false
       }
-    ]
+    ],
+    value: "",
   };
 
   //filter메소드 사용하여 할일 목록 지워 콘솔로그 찍기
   handleClick = (id) => {
     let newTodoData = this.state.todoData.filter((data) => data.id !== id);
-    console.log('newTodoData', newTodoData);
     this.setState({ todoData: newTodoData });
   }
 
+  handelChange = (e) => {
+    this.setState({ value: e.target.value });
+  }
+
+  handelSubmit = (e) => {
+    //form안에 input을 전송할 때 페이지 리로드 되는 걸 막아줌
+    e.preventDefault();
+
+    //새로운 할 일 데이터
+    let newTodo = {
+      id: Date.now(),
+      title: this.state.value,
+      complete: false,
+    };
+
+    //원래 있던 할 일에 새로운 할 일 더해주기
+    this.setState({ todoData: [...this.state.todoData, newTodo] });
+  }
   render() {
     return (
       //jsx에서는 html과 다르게 class뒤에 Name을 무조껀 붙혀야한다 'className'
@@ -55,6 +73,25 @@ export default class App extends Component {
               <button style={this.btnStyle} onClick={() => this.handleClick(data.id)}>x</button>
             </div>
           ))}
+
+
+          <form style={{ display: 'flex' }} onSubmit={this.handelSubmit}>
+            <input
+              type="text"
+              name="value"
+              style={{ flex: '10', padding: '5px' }}
+              placeholder="해야 할 일을 입력하세요."
+              value={this.state.value}
+              onChange={this.handelChange}
+            />
+            <input
+              type="submit"
+              value="입력"
+              className="btn"
+              style={{ flex: '1' }}
+            />
+          </form>
+
         </div>
       </div>
     )
